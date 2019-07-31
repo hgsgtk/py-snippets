2a9b743b2d054bae46153c30bb23627ca1bb427d
hgsgtk <hgsgtk@gmail.com>
2019-07-31 16:21:08 +0900

    output version of tensorflow

---
+++ tensorflow-tutorial/check_import.py
@@ -0,0 +1,7 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+from tensorflow import keras
+
+print(tf.version.VERSION)
+print(tf.keras.__version__)
